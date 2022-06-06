b2039bae12dd03ac37f4e5cbf07d7f28fb28039a
slimf1 <slimane.f41@gmail.com>
2022-06-06 22:26:06 +0200

    Eval command: Added context binding

--- src/main/kotlin/com/slim/ktpsbot/command/dev/Eval.kt
+++ src/main/kotlin/com/slim/ktpsbot/command/dev/Eval.kt
@@ -2,20 +2,24 @@ package com.slim.ktpsbot.command.dev
 
 import com.slim.ktpsbot.command.Command
 import com.slim.ktpsbot.context.Context
-import javax.script.ScriptEngineManager
+import javax.script.*
+
 
 class Eval : Command() {
     override val name: String = "eval"
     override val wlOnly: Boolean = true
     override val aliases: Iterable<String> = listOf("exec")
-    private val manager = ScriptEngineManager()
-    private val engine = manager.getEngineByName("js")
+    private val manager: ScriptEngineManager by lazy { ScriptEngineManager() }
+    private val engine: ScriptEngine by lazy { manager.getEngineByName("js") }
 
     override fun run(context: Context) {
         try {
-            val result = engine.eval(context.target)
+            val bindings = engine.getBindings(ScriptContext.ENGINE_SCOPE)
+            bindings.clear()
+            bindings["context"] = context
+            val result = engine.eval(context.target, bindings)
             context.reply("Result: $result")
-        } catch (exception: Exception) {
+        } catch (exception: ScriptException) {
             context.reply("An exception occurred: ${exception.message}")
         }
     }
--- src/test/kotlin/com/slim/ktpsbot/core/BotTest.kt
+++ src/test/kotlin/com/slim/ktpsbot/core/BotTest.kt
@@ -3,6 +3,7 @@ package com.slim.ktpsbot.core
 import com.slim.ktpsbot.core.mock.MockClient
 import com.slim.ktpsbot.utils.Resource
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
@@ -32,7 +33,7 @@ internal class BotTest {
     @Test
     fun testPingCommand() {
         mockClient?.receivedMessage(">franais\n|c:|1| RegularUser|-ping")
-        assertEquals(true, mockClient?.sentMessages?.isEmpty())
+        assertTrue(mockClient!!.sentMessages.isEmpty())
         mockClient?.receivedMessage(">franais\n|c:|1|+VoicedUser|-ping")
         assertEquals(1, mockClient?.sentMessages?.size)
         assertEquals("franais|pong", mockClient?.sentMessages?.get(0))
@@ -43,8 +44,17 @@ internal class BotTest {
 
     @Test
     fun testEvalCommand() {
+        mockClient?.receivedMessage(">franais\n|c:|1|#RoomOwner|-eval 5*3")
+        assertTrue(mockClient!!.sentMessages.isEmpty())
         mockClient?.receivedMessage(">botdevelopment\n|c:|1| Panur|-eval 4+2")
         assertEquals(1, mockClient?.sentMessages?.size)
         assertEquals("botdevelopment|Result: 6", mockClient?.sentMessages?.get(0))
+        mockClient?.receivedMessage("botdevelopment\n|c:|1| Panur|-eval context.sender.id")
+        assertEquals("botdevelopment|Result: panur", mockClient?.sentMessages?.get(1))
+        mockClient?.receivedMessage("botdevelopment\n|c:|1| Panur|-eval context.reply('test')")
+        assertEquals(4, mockClient?.sentMessages?.size)
+        assertEquals("botdevelopment|test", mockClient?.sentMessages?.get(2))
+        mockClient?.receivedMessage("botdevelopment\n|c:|1| Panur|-eval context.propertyThatDoesntExist.id")
+        assertTrue(mockClient!!.sentMessages.last().startsWith("botdevelopment|An exception occurred"))
     }
 }
